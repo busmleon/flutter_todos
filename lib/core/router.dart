cc540526e3d7babd885c7e8391fe62e0eb046d5e
busmleon <leon.schoolmail@web.de>
2021-04-05 00:29:55 +0200

    set HomePage to default route; deleted wrong route page

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -4,22 +4,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../pages/home/home.page.dart';
 import '../states/home/home.page.bloc.dart';
 import 'dependency.injector.dart';
-import 'widgets/error.route.widget.dart';
 
 class MyRouter {
   const MyRouter();
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
+      default:
         return MaterialPageRoute(
           builder: (_) => BlocProvider(
             create: (_) => injector<HomePageBloc>(),
             child: const HomePage(),
           ),
         );
-
-      default:
-        return MaterialPageRoute(builder: (_) => const ErrorRouteWidget());
     }
   }
 }
